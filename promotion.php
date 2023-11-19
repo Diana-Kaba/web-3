fb16f95ee816c643f2075c3370e13862d63ad2d4
Diana <kabadiana327@gmail.com>
2023-11-19 19:10:57 +0200

    Added task promotion

---
+++ promotion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Акція</title>
+    <style>
+        i {
+            color: coral;
+        }
+    </style>
+</head>
+<body>
+<?php
+$now = time();
+$nowDate = date("d.m.Y H:i", $now);
+$future = mktime(0, 0, 0, 12, 19, 2023);
+
+$diff = $future - $now;
+$days = round($diff / (24 * 60 * 60), 0);
+$hours = round($diff / 60, 0);
+$minutes = round($diff / (60 * 60), 0);
+
+echo "<h1>Акція закінчиться через <i>$days днів $hours годин $minutes хвилин</i></h1>";
+echo "<h2>Сьогодні <i>$nowDate</i></h2>";
+?>
+
+</body>
+</html>
