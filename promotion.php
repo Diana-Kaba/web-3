384ed242ad7c7f95e9f34bc81c013ace89de68ab
Diana <kabadiana327@gmail.com>
2023-11-21 11:18:50 +0200

    Changed hours, minutes and timezone

--- promotion.php
+++ promotion.php
@@ -12,14 +12,16 @@
 </head>
 <body>
 <?php
+date_default_timezone_set('Europe/Kyiv');
+
 $now = time();
 $nowDate = date("d.m.Y H:i", $now);
 $future = mktime(0, 0, 0, 12, 19, 2023);
 
 $diff = $future - $now;
 $days = round($diff / (24 * 60 * 60), 0);
-$hours = round($diff / 60, 0);
-$minutes = round($diff / (60 * 60), 0);
+$hours = round($diff / (60 * 60), 0);
+$minutes = round($diff / 60, 0);
 
 echo "<h1>Акція закінчиться через <i>$days днів $hours годин $minutes хвилин</i></h1>";
 echo "<h2>Сьогодні <i>$nowDate</i></h2>";
